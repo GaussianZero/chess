9130876a7608936484fc6d6dfabc3f8f0f342be0
gaussian.zero <gaussian.zero>
2019-10-13 22:29:13 -0700

    add logging

--- notation.go
+++ notation.go
@@ -130,6 +130,7 @@ func (_ AlgebraicNotation) Encode(pos *Position, m *Move) string {
 // Decode implements the Decoder interface.
 func (_ AlgebraicNotation) Decode(pos *Position, s string) (*Move, error) {
 	s = removeSubstrings(s, "?", "!", "+", "#", "e.p.", "=")
+	fmt.Printf("Checking All valid moves for position %v, %s\n ", pos, s)
 	for _, m := range pos.ValidMoves() {
 		str := AlgebraicNotation{}.Encode(pos, m)
 		str = removeSubstrings(str, "?", "!", "+", "#", "e.p.", "=")
